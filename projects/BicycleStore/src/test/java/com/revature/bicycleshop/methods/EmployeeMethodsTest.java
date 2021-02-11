4ad6a1eba3b04358749d36fd4037b75c918ab842
Haley Moses <vhr825@gmail.com>
2021-02-10 23:14:38 -0600

    committing my git code

---
+++ projects/BicycleStore/src/test/java/com/revature/bicycleshop/methods/EmployeeMethodsTest.java
@@ -0,0 +1,14 @@
+package com.revature.bicycleshop.methods;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeMethodsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ projects/BicycleStore/src/test/java/com/revature/bicycleshop/methods/ManagerMethodsTest.java
@@ -0,0 +1,14 @@
+package com.revature.bicycleshop.methods;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ManagerMethodsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
